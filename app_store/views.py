260f3c644715e2e739a8adefc75acd420efc0a4d
yakov <mr.afonichkin2018@gmail.com>
2025-12-20 15:48:29 +0300

    completed

--- app_store/views.py
+++ app_store/views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse, HttpResponse, HttpResponseNotFound
+from django.shortcuts import render
 from .models import DATABASE
 from app_store.logic.services import filtering_category
 from app_store.logic.control_cart import view_in_cart, add_to_cart, remove_from_cart
@@ -31,9 +32,18 @@ def product_view_json(request):
 
 def shop_view(request):
     if request.method == 'GET':
-        with open('app_store/shop.html', 'r', encoding='utf8') as f:
-            data = f.read()
-        return HttpResponse(data)
+        category_key = request.GET.get('category')
+        ordering_key = request.GET.get('ordering')
+        if ordering_key:
+            if request.GET.get('reverse').lower() == 'true':
+                data = filtering_category(DATABASE, category_key, ordering_key, True)
+            else:
+                data = filtering_category(DATABASE, category_key, ordering_key)
+        else:
+            data = filtering_category(DATABASE, category_key)
+        return render(request, 'app_store/shop.html',
+                      context={'products': data,
+                               "category": category_key})
 
 
 def product_page_view(request, page):
@@ -42,15 +52,17 @@ def product_page_view(request, page):
         if isinstance(page, str):
             for data in DATABASE.values():
                 if data['html'] == page:
-                    with open(f'app_store/product/{page}.html', encoding='utf-8') as f:
-                        return HttpResponse(f.read())
-            return HttpResponse(status=404)
+                    data_other_products = DATABASE.values()
+                    return render(request, 'app_store/product.html', context={'product': data,
+                                                                                            'other_products': data_other_products})
+                return HttpResponse(status=404)
 
         elif isinstance(page, int):
             data = DATABASE.get(str(page))
             if data:
-                with open(f'app_store/product/{data['html']}.html', encoding='utf-8') as f:
-                    return HttpResponse(f.read())
+                data_other_products = DATABASE.values()
+                return render(request, 'app_store/product.html', context={'product': data,
+                                                                                        'other_products': data_other_products})
             return HttpResponse(status=404)
 
 
@@ -86,3 +98,17 @@ def cart_del_view_json(request, id_product):
         return JsonResponse({"answer": "Неудачное удаление из корзины"},
                             status=404,
                             json_dumps_params={'ensure_ascii': False})
+
+
+def cart_view(request):
+    if request.method == 'GET':
+        username = ''
+        data = view_in_cart(username)[username]
+
+        products = []
+        for product_id, quantity in data['products'].items():
+            product = DATABASE[product_id]
+            product['quantity'] = quantity
+            product['price_total'] = product['price_after'] * quantity
+            products.append(product)
+        return render(request, 'app_store/cart.html', context={'products': products} )
